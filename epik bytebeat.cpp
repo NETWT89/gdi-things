adacb68d0f4fe78abc8c50159bab330ec0548cc2
Kavru <cacitacacita808@gmail.com>
2025-10-03 00:33:29 -0500

    Bytebeat

---
+++ epik bytebeat.cpp
@@ -0,0 +1,26 @@
+// list of bytebeat that is free to use (you still have to give credit to kavru for it tho.)
+
+
+t*2 & t<<2 & t>>8
+
+
+
+t*3 & t- & t*3 & t<<3
+
+
+
+t*(t^t>>20*(t>>11))
+
+
+
+t*3 & t>>3 & t<<3 & t^540 & t*430
+
+
+
+t*3 & t<<3 & t>>3 & t*430 & t++ & t*(4300>>t*900 & t*4300) & t*(8900) & t^429 & t++
+
+
+
+((t>>5&t)-(t>>5)+(t>>5&t))+(t*((t>>14)&14))
+
+// more coming soon.
